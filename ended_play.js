c218e929e30281468cf938ea8fe8e962f1999fd5
peterstrzk <peterstrzk@gmail.com>
2021-09-08 20:19:39 +0200

    today

--- ended_play.js
+++ ended_play.js
@@ -1,11 +1,9 @@
-let clicked = false;
-function done(){
-  if (document.getElementById("end_game").clicked){
-    
-  document.getElementById("body").style.background = "green";
-  
+if (sessionStorage.getItem('tso') == 'true') {
+  document.getElementById("first").style.background = "green";
+  console.log(sessionStorage)
+}
 
-  }
-};
+document.getElementById("end_game").addEventListener("click",  () => 
+  (sessionStorage.setItem('tso', 'true'))
+)
 
-document.getElementById("end_game").addEventListener("click", function() { clicked = true;}) 
