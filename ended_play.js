29c6051745eb4dbf1fbc3b258de5b5ea918925bf
peterstrzk <peterstrzk@gmail.com>
2021-09-08 18:32:29 +0200

    today

---
+++ ended_play.js
@@ -0,0 +1,11 @@
+let clicked = false;
+function done(){
+  if (document.getElementById("end_game").clicked){
+    
+  document.getElementById("body").style.background = "green";
+  
+
+  }
+};
+
+document.getElementById("end_game").addEventListener("click", function() { clicked = true;}) 
--- func_metric.js
+++ func_metric.js
@@ -1,47 +1,45 @@
 
+//wiek osoby badanej//
+function form_age(e) {
+  let inputVal = document.getElementById("age").value;
 
-function metryczka(e) {
-    let inputVal = document.getElementById("age").value;
-    
-    
-    if (inputVal >= 99 ){
-        e.preventDefault();
-        window.alert("dobra chuju koniec zartów")
-        
-        
-    }
-    else if (inputVal >= 18) {
-        
-    }
-    else  {
-        e.preventDefault();
-        window.alert("Musisz mieć skończone 18 lat")
-        
-        
-    }
-
-
+  if (inputVal >= 99) {
+    e.preventDefault();
+    window.alert("Proszę sobie nie żartować! Wpisz swój wiek poprawnie!");
+  } else if (inputVal >= 18) {
+  } else if (inputVal == 0) {
+    e.preventDefault();
+    window.alert("wpisz swój wiek!");
+  } else {
+    e.preventDefault();
+    window.alert("Musisz mieć skończone 18 lat");
+  }
 }
+
+//reszta metryczki wykształcenie, zamieszkanie, płeć//
+
 function form_select(e) {
-    let inputSelect = document.getElementById("gender").value;
-    
-    
-    if (inputSelect==true){
-        window.alert(inputSelect)
-        ++inputSelect;
-    }
-    else {
-        window.alert("false")
-    }
+  let inputSelect = [
+    document.getElementById("gender").value,
+    document.getElementById("education").value,
+    document.getElementById("acomodation").value,
+  ];
 
-    return inputSelect;
+  if (inputSelect[(0, 1, 2)] == true) {
+  } else {
+    e.preventDefault();
+    window.alert("Zaznacz wszystkie wymagane opcje!");
+  }
 }
 
-
+//funkcja z dwóch funkcji//
 function call_metric(e) {
-    metryczka(e)
-    form_select(e)
-    
-
+  form_age(e);
+  form_select(e);
 }
-document.getElementById("btn_next").addEventListener("click", (e) => call_metric(e))
+
+//wywołujemy funckje//
+
+document
+  .getElementById("btn_next")
+  .addEventListener("click", (e) => call_metric(e));
